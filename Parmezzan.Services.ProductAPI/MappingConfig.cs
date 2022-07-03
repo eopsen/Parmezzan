405c08220737800455d79ab2f5192b8abff04cbe
maciej <maciej@hostelsystem.com>
2022-07-03 21:52:22 +0200

    add product repository

---
+++ Parmezzan.Services.ProductAPI/MappingConfig.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using Parmezzan.Services.ProductAPI.Models;
+using Parmezzan.Services.ProductAPI.Repository.Dto;
+
+namespace Parmezzan.Services.ProductAPI
+{
+    public class MappingConfig
+    {
+        public static MapperConfiguration RegisterMaps()
+        {
+            return new MapperConfiguration(config =>
+            {
+                config.CreateMap<ProductDto, Product>();
+                config.CreateMap<Product, ProductDto>();
+            });
+        }
+    }
+}
---
+++ Parmezzan.Services.ProductAPI/Repository/ProductRepository.cs
@@ -0,0 +1,72 @@
+﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using Parmezzan.Services.ProductAPI.DbContextes;
+using Parmezzan.Services.ProductAPI.Models;
+using Parmezzan.Services.ProductAPI.Repository.Dto;
+
+namespace Parmezzan.Services.ProductAPI.Repository
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly ApplicationDbContext _db;
+        private IMapper _mapper;
+
+        public ProductRepository(ApplicationDbContext db, IMapper mapper)
+        {
+            _db = db;
+            _mapper = mapper;
+        }
+
+        public async Task<ProductDto> CreateUpdateProduct(ProductDto productDto)
+        {
+            Product product = _mapper.Map<ProductDto, Product>(productDto);
+
+            if (product.ProductId > 0)
+            {
+                _db.Products.Update(product);
+            } 
+            else
+            {
+                _db.Products.Add(product);
+            }
+
+            await _db.SaveChangesAsync();
+
+            return _mapper.Map<Product, ProductDto>(product);
+        }
+
+        public async Task<bool> DeleteProduct(int productId)
+        {
+            try
+            {
+                Product product = await _db.Products
+                    .FirstOrDefaultAsync(p => p.ProductId == productId);
+
+                if (product == null) return false;
+
+                _db.Products.Remove(product);
+                await _db.SaveChangesAsync();
+
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        public async Task<ProductDto> GetProductById(int productId)
+        {
+            Product product = await _db.Products
+                .FirstOrDefaultAsync(p => p.ProductId == productId);
+
+            return _mapper.Map<ProductDto>(product);
+        }
+
+        public async Task<IEnumerable<ProductDto>> GetProducts()
+        {
+            List<Product> productList = await _db.Products.ToListAsync();
+            return _mapper.Map<List<ProductDto>>(productList);
+        }
+    }
+}
