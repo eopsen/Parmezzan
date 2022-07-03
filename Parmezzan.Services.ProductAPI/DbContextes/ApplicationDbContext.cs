a182d4e81b938ff72d9ff8141e1df3dccec8e166
maciej <maciej@hostelsystem.com>
2022-07-03 21:25:14 +0200

    Add product table

---
+++ Parmezzan.Services.ProductAPI/DbContextes/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Parmezzan.Services.ProductAPI.Models;
+
+namespace Parmezzan.Services.ProductAPI.DbContextes
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base (options)
+        {
+
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
