6a40e1b496760b08340a776c7986f171c3e59a83
maciej <maciej@hostelsystem.com>
2022-07-03 20:55:08 +0200

    Add project files.

---
+++ Parmezzan.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Parmezzan.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
