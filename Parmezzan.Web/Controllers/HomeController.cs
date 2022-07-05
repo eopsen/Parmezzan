0bcd95a92d77218b6879bedbc5f220eaf24a1018
maciej <maciej@hostelsystem.com>
2022-07-05 18:59:11 +0200

    show products on home page

--- Parmezzan.Web/Controllers/HomeController.cs
+++ Parmezzan.Web/Controllers/HomeController.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using Parmezzan.Web.Models;
+using Parmezzan.Web.Models.Dto;
+using Parmezzan.Web.Services.IServices;
 using System.Diagnostics;
 
 namespace Parmezzan.Web.Controllers
@@ -8,15 +11,24 @@ namespace Parmezzan.Web.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IProductService _productService;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IProductService productService)
         {
             _logger = logger;
+            _productService = productService;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            var result = new List<ProductDto>();
+            var response = await _productService.GetAllProductsAsync<ResponseDto>("");
+            if (response != null && response.IsSuccess)
+            {
+                result = JsonConvert.DeserializeObject<List<ProductDto>>(Convert.ToString(response.Result));
+            }
+
+            return View(result);
         }
 
         public IActionResult Privacy()
