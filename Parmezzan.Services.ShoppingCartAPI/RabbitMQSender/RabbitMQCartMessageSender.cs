076f3261adbd7f2542ff1ce61ae6ee65f98b8254
maciej <maciej@hostelsystem.com>
2022-07-07 00:31:50 +0200

    add rabbitmq nuget to project

---
+++ Parmezzan.Services.ShoppingCartAPI/RabbitMQSender/RabbitMQCartMessageSender.cs
@@ -0,0 +1,12 @@
+﻿using Parmezzan.MessageBus;
+
+namespace Parmezzan.Services.ShoppingCartAPI.RabbitMQSender
+{
+    public class RabbitMQCartMessageSender : IRabbitMQCartMessageSender
+    {
+        public void SendMessage(BaseMessage baseMessage, string queueName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
