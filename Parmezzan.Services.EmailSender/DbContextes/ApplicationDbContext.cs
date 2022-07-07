1ae1ee2f726f490588253cdad2a36e759b4c344c
maciej <maciej@hostelsystem.com>
2022-07-07 18:31:41 +0200

    add email sender service

---
+++ Parmezzan.Services.EmailSender/DbContextes/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Parmezzan.Services.EmailSender.Models;
+
+namespace Parmezzan.Services.EmailSender.DbContextes
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<EmailLog> EmailLogs { get; set; }
+    }
+}
